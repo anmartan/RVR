fcd5b897b6784f8bb25131c8371d30467d32dc36
Ana <anmartan@ucm.es>
2021-05-26 23:48:34 +0200

    Ejercicio 1 hecho

--- Practica2.2/rvr-release1.0/practica2.2/serializacion/ejercicio1.cc
+++ Practica2.2/rvr-release1.0/practica2.2/serializacion/ejercicio1.cc
@@ -18,8 +18,8 @@ private:
 public:
     Jugador(const char * _n, int16_t _x, int16_t _y):pos_x_(_x),pos_y_(_y)
     {
-        _size = 2 * sizeof(int16_t) + MAX_NAME * sizeof(char);
         strncpy(name_, _n, MAX_NAME);
+        _size = 2 * sizeof(int16_t) + MAX_NAME * sizeof(char);
     };
 
     virtual ~Jugador(){};
@@ -31,32 +31,30 @@ public:
 
         size_t offset = 0;
         // guardamos las variables
-        memcpy(_data + offset, name_, MAX_NAME);
+        memcpy(_data + offset, name_, MAX_NAME * sizeof(char));
         offset += MAX_NAME;
 
         memcpy(_data + offset, &pos_x_, sizeof(int16_t));
         offset +=  sizeof(int16_t);
 
         memcpy(_data + offset, &pos_y_,  sizeof(int16_t));
-        offset +=  sizeof(int16_t);
     }
 
     int from_bin(char * data)
     {
         // si el tamano del data no es el correcto, devolvemos -1
-        if(sizeof(_data) > sizeof(char) * MAX_NAME + 2 * sizeof(int16_t))
+        if(sizeof(data) > sizeof(char) * MAX_NAME + 2 * sizeof(int16_t))
             return -1;
             
         size_t offset = 0;
         // guardamos las variables
-        memcpy(name_, _data + offset, sizeof(char) * MAX_NAME);
+        memcpy(name_, data + offset, sizeof(char) * MAX_NAME);
         offset += sizeof(char) * MAX_NAME;
 
-        memcpy(&pos_x_, _data + offset, sizeof(int16_t));
+        memcpy(&pos_x_, data + offset, sizeof(int16_t));
         offset +=  sizeof(int16_t);
 
-        memcpy(&pos_y_, _data + offset, sizeof(int16_t));
-        offset +=  sizeof(int16_t);
+        memcpy(&pos_y_, data + offset, sizeof(int16_t));
         return 0;
     }
 
@@ -77,29 +75,35 @@ int main(int argc, char **argv)
     one_w.to_bin();
 
     // 2. Escribir la serialización en un fichero
-    int file = open("./player.txt", O_RDWR | O_CREAT);
+    int file = open("./player.player", O_RDWR | O_CREAT, 0666);
     if(file == -1)
     {
-        printf("Error al serializar el archivo\n");
+        printf("Error al crear el archivo de escritura\n");
         return -1;
     }
     write(file, one_w.data(), one_w.size());
     close(file);
 
     // 3. Leer el fichero
-    file = open("./player.txt", O_RDONLY, 0666);
+    file = open("./player.player", O_RDONLY, 0666);
     if(file == -1)
     {
-        printf("Error al leer el archivo\n");
+        printf("Error al abrir el archivo de lectura\n");
         return -1;
     }
 
     // 4. "Deserializar" en one_r
     char* buffer = (char*)malloc(one_r.size());
+    int bytes = read(file, buffer, one_r.size());
+    close(file);
+    if(bytes == -1)
+    {
+        printf("Error al leer el archivo\n");
+    }
     one_r.from_bin(buffer);
 
     // 5. Mostrar el contenido de one_r
-    printf("Nombre del jugador: %s \nPosicion en x: %u \nPosicion en y: %u\n", one_r.name_, one_r.pos_x_, one_r.pos_y_);
+    printf("Nombre del jugador: %s \nPosicion en x: %d \nPosicion en y: %d\n", one_r.name_, one_r.pos_x_, one_r.pos_y_);
     return 0;
 }
 
